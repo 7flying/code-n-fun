df6e22db1543c9c64dfc2c0f1290458773fc1fa1
7flying <diez.f.irene@gmail.com>
2015-06-27 18:20:59 +0200

    Setup a stack, test C code

---
+++ little-os/kmain.c
@@ -0,0 +1,4 @@
+
+int kmain(int arg0, int arg1) {
+  return arg0 + arg1;
+}
--- little-os/loader.asm
+++ little-os/loader.asm
@@ -1,9 +1,17 @@
 ;;
 global loader
 
+extern kmain
+
 MAGIC_NUMBER equ 0x1BADB002
 FLAGS equ 0x0
 CHECKSUM equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE equ 4096
+  
+section .bss
+align 4
+kernel_stack:
+        resb KERNEL_STACK_SIZE
 
 section .text:
 align 4
@@ -13,5 +21,10 @@ align 4
 
 loader:
         mov eax, 0xCAFEBABE
+        mov esp, kernel_stack + KERNEL_STACK_SIZE
+        push dword 10
+        push dword 13
+        call kmain
 .loop:
         jmp .loop
+
