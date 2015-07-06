c0c9c68bbb8c7d4d9532a2fcae5e5159c87702d0
7flying <diez.f.irene@gmail.com>
2015-07-06 22:26:39 +0200

    Load GDT

--- little-os/segment.asm
+++ little-os/segment.asm
@@ -1,6 +1,15 @@
-global lgdts
+global load_gdt
 
-lgdts:
+load_gdt:
         mov eax, [esp + 4]
         lgdt [eax]
+load_segment_reg:
+        mov ds, 0x10
+        mov ss, 0x10
+        mov es, 0x10
+        mov gs, 0x10
+        mov fs, 0x10
+; To load cs do a far jump
+        jmp 0x08:flush_cs
+flush_cs:
         ret
--- little-os/segment.h
+++ little-os/segment.h
@@ -1,16 +1,19 @@
 #ifndef _SEGMENT_H_
 #define _SEGMENT_H_
 
+
 struct GDT {
-    unsigned int address;
-    unsigned short size;
+    /* Size of GDT */
+    unsigned short limit;
+    /* Starting address for the GDT*/
+    unsigned int base;
 } __attribute__((packed));
 
 /**
- * lgdts:
+ * load_gdt:
  * Loads the GDT into the processor.
  * @param gdt - GDT struct with address and size to be loaded in the processor
  */
-void lgdts(struct GDT);
+void load_gdt(struct GDT);
 
 #endif /* _SEGMENT_H_ */
