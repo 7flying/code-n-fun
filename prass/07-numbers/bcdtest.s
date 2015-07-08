b0fa45481ace6c28eb39ee8f5d648935a76d4816
7flying <diez.f.irene@gmail.com>
2015-07-08 22:54:07 +0200

    Finish integer operations

---
+++ prass/07-numbers/bcdtest.s
@@ -0,0 +1,16 @@
+    # bcdtest.s - example of BCD integer values
+    .section .data
+data1:
+    .byte 0x34, 0x12, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+data2:
+    .int 2
+    .section .text
+    .globl _start
+_start:
+    fbld data1
+    fimul data2
+    fbstp data1
+    movl $1, %eax
+    movl $0, %ebx
+    int %0x80
+    
---
+++ prass/07-numbers/settest.s
@@ -0,0 +1,15 @@
+    # settest.s - example of 128 bit SSE registers
+    .section .data
+values1:
+    .int 1, -1, 0, 135246
+values2:
+    .quad 1, -1
+    .section .text
+    .globl _start
+_start:
+    movdqu values1, %xmm0
+    movdqu values2, %xmm1
+    movl $1, %eax
+    movl $0, %ebx
+    int $0x80
+    
